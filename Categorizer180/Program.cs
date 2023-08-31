187d075dd5e4cbc4fa0cb92acb206796abc5254b
Jordi Bargalló <bargallo@gmail.com>
2023-08-31 13:30:50 +0200

    Primera versión consumidor ML.Net

---
+++ Categorizer180/Program.cs
@@ -0,0 +1,45 @@
+﻿using Categorizer180;
+using Microsoft.ML;
+using Microsoft.ML.Data;
+
+Console.WriteLine("=============== Incio Proceso: " + DateTime.Now.ToString() + "===============");
+var str = "INCIDENCIA BBDD CLIENTE 918019 1#Código Tipología: T1 2#Código Tipo de Petición: ST96 3#Código Entidad/Mediador: G65089 4#Código Peticionario:  5#Mail Peticionario: robotrpa1.es@generali.com       6#Teléfono Peticionario: 7#Cliente (DNI) (Opcional): 8#Póliza (RamoCiaPóliza) (Opcional): 9#Aplicación (Opcional):  10#Agrupación Ramo:  11#Observaciones: INCIDENCIA: 918019  con mediador: 65089 ######";
+float maxScore;
+//Dato de ejemplo a procesar
+var sampleData = new MLModel1.ModelInput()
+{
+    Input = @str,
+};
+
+//Cargar el modelo y realizar la predicción para el dato de ejemplo
+var result = MLModel1.Predict(sampleData, Path.GetFullPath("MLModel1.mlnet"));
+
+
+//Mostrar los rsultados por consola estandar
+PrintResults(result, str);
+
+// Resultados ordenados por categoria
+//var result2 = MLModel1.PredictAllLabels(sampleData, Path.GetFullPath("MLModel1.mlnet"));
+//Console.WriteLine("{" + string.Join(",\n ", result2.Select(m => $"{m.Key}={m.Value*100}")) + "}");
+//Console.WriteLine("{" + string.Join(", ", result2) + "}");
+
+Console.WriteLine("=============== Fin Proceso: " + DateTime.Now.ToString() + "===============");
+  void PrintResults(MLModel1.ModelOutput result, String str)
+{
+    Console.WriteLine("Path del Modelo:" + Path.GetFullPath("MLModel1.mlnet"));
+    Console.WriteLine(" Ejemplo: " + str[..25]);
+    Console.WriteLine(" Categorias: " + result.Score.Length);
+    Console.WriteLine(" Predicción: " + result.PredictedLabel);
+    maxScore = getMaxScore(result);
+    Console.WriteLine($"El máximo valor es {maxScore} y representa el {(maxScore / 1) * 100}% del total.");
+
+}
+
+static float getMaxScore(MLModel1.ModelOutput result)
+{
+    float maxScore = 0;
+    foreach (float score in result.Score)
+        if (score > maxScore)
+            maxScore = score;
+    return maxScore;
+}
--- README.md
+++ README.md
@@ -1,2 +1,52 @@
-# Categorizer
-Clases consumidoras de un modelo ML.Net con un entranamiento FastTree
+
+# Proyecto Categorizer de Predicción con ML.Net
+
+Este proyecto implementa un modelo de Machine Learning en .NET 6.0 utilizando la librería ML.Net de Microsoft.
+
+## Descripción 
+
+El objetivo es predecir una variable de salida pasando inputs de ejemplo a un modelo entrenado previamente. El modelo implementado es un árbol de decisión FastTree entrenado con un dataset (entreno previo).
+
+La aplicación consume el modelo y permite al usuario pasar nuevos ejemplos en formato String para obtener una predicción.
+
+La finalidad es generar una libreria que pueda ser consumida desde Uipath mediante por ejemplo, un Invoke Code
+
+## Características
+
+- Uso de ML.Net para entrenar e implementar un modelo FastTree
+- Carga del modelo entrenado desde un archivo .csv
+- Método para realizar predicciones pasando nuevos datos 
+- Ejemplos de uso y pruebas
+
+## Dependencias
+
+- .NET 6.0
+- ML.Net 2.0.1 
+- Microsoft.ML.FastTree 2.0.1 
+- Dataset de entrenamiento (incluido)
+
+## Ejecución
+
+Para ejecutar el proyecto:
+
+1. Clona este repositorio
+2. Abre la solución en Visual Studio 2022
+3. Restaura los paquetes de NuGet
+4. Compila la solución
+5. Ejecuta la aplicación consola 
+6. Prueba pasando nuevos datos de ejemplo
+
+## Contribuciones
+
+Las contribuciones y mejoras son bienvenidas! Por favor abre un issue o pull request en GitHub. 
+
+## Licencia  
+
+Este proyecto tiene licencia MIT.
+
+## Contacto
+
+Autor: Jordi Bargalló
+LinkedIn: https://es.linkedin.com/in/bargallo
+Correo: bargallo@gmail.com
+
